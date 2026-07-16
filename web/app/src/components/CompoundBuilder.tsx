da7f5df02b5e0ae6cc4599ce8096b67028e7ef51
David Jones <david@dgjones.info>
2026-07-16 11:38:08 -0400

    fix: escape quotes in CompoundBuilder escapeHtml (CodeQL #272)

--- web/app/src/components/CompoundBuilder.tsx
+++ web/app/src/components/CompoundBuilder.tsx
@@ -27,7 +27,7 @@ const BRIDGE_COLORS = [
 ];
 
 function escapeHtml(s: string): string {
-  return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+  return s.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#39;");
 }
 
 function renderCompoundText(text: string, spans: SpanRangePayload[], sourceTitles: Record<number, string>): string {
